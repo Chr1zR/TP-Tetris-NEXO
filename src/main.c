fa38562df145ca1598bc912752d22fedcd085dfb
agusbuso <agustinbusolin@gmail.com>
2026-05-27 22:05:19 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,11 +4,6 @@ DNI: 41332491
 Usuario: Chr1z
 Entrega: Sí
 
-Apellido: Vega, Marcos Julian Aguero
-DNI: 12345678
-Usuario: OsoMark
-Entrega: Sí
-
 Apellido: Busolín, Agustín Osvaldo
 DNI: 44792752
 Usuario: agusbuso
